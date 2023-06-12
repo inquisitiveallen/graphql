1809aec6342e1cc52ca7c72c91c15ae3815e2dcb
inquisitiveallen <allenthundiyil@gmail.com>
2023-06-12 17:06:12 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
+
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers/index')
+const { MONGODB } = require('./config.js')
+
+
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDB connected')
+        return server.listen({ port: 5000 })
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
